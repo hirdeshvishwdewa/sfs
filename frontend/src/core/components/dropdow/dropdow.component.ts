ace622d136a3fb62e08bc16e4eba2d6f6955c7e2
Hirdesh Vishwdewa <hirdeshvishwdewa@gmail.com>
2024-12-26 16:39:10 +0530

    Setting default value on change of language.

--- frontend/src/core/components/dropdow/dropdow.component.ts
+++ frontend/src/core/components/dropdow/dropdow.component.ts
@@ -14,6 +14,7 @@ export class DropdownComponent {
     optionClicked = new EventEmitter<any>();
 
     optionClickHandler(option: any) {
+        this.defaultValue = option;
         this.optionClicked.emit(option);
     }
 }
